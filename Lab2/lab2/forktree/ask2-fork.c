cb1cfc7bb53ee78b819190079f0ea396c9620b6c
Marios Kerasiotis <marioskerasiotis@gmail.com>
2020-04-29 15:53:27 +0300

    added report

--- Lab2/lab2/forktree/ask2-fork.c
+++ Lab2/lab2/forktree/ask2-fork.c
@@ -1,9 +1,9 @@
-#include <unistd.h>
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 #include "proc-common.h"
 
@@ -20,91 +20,94 @@
  * A-+-B---D
  *   `-C
  */
-void fork_procs(void)
-{
-	/*
-	 * initial process is A.
-	 */
-	pid_t pid_b, pid_c, pid_d;
-	int status;
+void fork_procs(void) {
+  /*
+   * initial process is A.
+   */
+  pid_t pid_b, pid_c, pid_d;
+  int status;
 
-	change_pname("A");
-	printf("Proccess A has PID = %ld\n", (long)getpid());
+  change_pname("A");
+  printf("Proccess A has PID = %ld\n", (long)getpid());
 
-	// Code bellow is for child B
-	fprintf(stderr, "Parent A, PID = %ld: Creating child B\n", (long)getpid());
-	pid_b = fork();
-	if (pid_b < 0)
-	{
-		/* fork failed */
-		perror("B: fork");
-		exit(1);
-	}
+  // Code bellow is for child B
+  fprintf(stderr, "Parent A, PID = %ld: Creating child B\n", (long)getpid());
+  pid_b = fork();
+  if (pid_b < 0) {
+    /* fork failed */
+    perror("B: fork");
+    exit(1);
+  }
 
-	if (pid_b == 0)
-	{
-		change_pname("B");
-		printf("Proccess B has PID = %ld\n", (long)getpid());
-		fprintf(stderr, "Parent B, PID = %ld: Creating child D\n", (long)getpid());
-		pid_d = fork();
-		if (pid_d < 0)
-		{
-			/* fork failed */
-			perror("D: fork");
-			exit(1);
-		}
+  if (pid_b == 0) {
+    change_pname("B");
+    printf("Proccess B has PID = %ld\n", (long)getpid());
+    fprintf(stderr, "Parent B, PID = %ld: Creating child D\n", (long)getpid());
+    pid_d = fork();
+    if (pid_d < 0) {
+      /* fork failed */
+      perror("D: fork");
+      exit(1);
+    }
 
-		if (pid_d == 0)
-		{
-			change_pname("D");
-			printf("Proccess D has PID = %ld\n", (long)getpid());
-			printf("D: Sleeping...\n");
-			sleep(SLEEP_PROC_SEC);
-			printf("D: Done Sleeping...\n");
-			printf("D with PID = %ld is ready to terminate...\nD: Exiting...\n", (long)getpid());
-			exit(EXIT_D);
-		}
+    if (pid_d == 0) {
+      change_pname("D");
+      printf("Proccess D has PID = %ld\n", (long)getpid());
+      printf("D: Sleeping...\n");
+      sleep(SLEEP_PROC_SEC);
+      printf("D: Done Sleeping...\n");
+      printf("D with PID = %ld is ready to terminate...\nD: Exiting...\n",
+             (long)getpid());
+      exit(EXIT_D);
+    }
 
-		printf("Parent B, PID = %ld: Created child D with PID = %ld, waiting for it to terminate...\n",
-			   (long)getpid(), (long)pid_d);
-		pid_d = wait(&status);
-		explain_wait_status(pid_d, status);
-		printf("B with PID = %ld is ready to terminate...\nB: Exiting...\n", (long)getpid());
-		exit(EXIT_B);
-	}
+    printf(
+        "Parent B, PID = %ld: Created child D with PID = %ld, waiting for it "
+        "to terminate...\n",
+        (long)getpid(), (long)pid_d);
+    pid_d = wait(&status);
+    explain_wait_status(pid_d, status);
+    printf("B with PID = %ld is ready to terminate...\nB: Exiting...\n",
+           (long)getpid());
+    exit(EXIT_B);
+  }
 
-	// Code bellow is for child C
-	fprintf(stderr, "Parent A, PID = %ld: Creating child C\n", (long)getpid());
-	pid_c = fork();
-	if (pid_c < 0)
-	{
-		/* fork failed */
-		perror("C: fork");
-		exit(1);
-	}
+  // Code bellow is for child C
+  fprintf(stderr, "Parent A, PID = %ld: Creating child C\n", (long)getpid());
+  pid_c = fork();
+  if (pid_c < 0) {
+    /* fork failed */
+    perror("C: fork");
+    exit(1);
+  }
 
-	if (pid_c == 0)
-	{
-		change_pname("C");
-		printf("Proccess C has PID = %ld\n", (long)getpid());
-		printf("C: Sleeping...\n");
-		sleep(SLEEP_PROC_SEC);
-		printf("C: Done Sleeping...\n");
-		printf("C with PID = %ld is ready to terminate...\nC: Exiting...\n", (long)getpid());
-		exit(EXIT_C);
-	}
+  if (pid_c == 0) {
+    change_pname("C");
+    printf("Proccess C has PID = %ld\n", (long)getpid());
+    printf("C: Sleeping...\n");
+    sleep(SLEEP_PROC_SEC);
+    printf("C: Done Sleeping...\n");
+    printf("C with PID = %ld is ready to terminate...\nC: Exiting...\n",
+           (long)getpid());
+    exit(EXIT_C);
+  }
 
-	printf("Parent A, PID = %ld: Created child C with PID = %ld, waiting for it to terminate...\n",
-		   (long)getpid(), (long)pid_c);
-	pid_c = wait(&status);
-	explain_wait_status(pid_c, status);
+  printf(
+      "Parent A, PID = %ld: Created child C with PID = %ld, waiting for it to "
+      "terminate...\n",
+      (long)getpid(), (long)pid_c);
+  pid_c = wait(&status);
+  explain_wait_status(pid_c, status);
 
-	printf("Parent A, PID = %ld: Created child B with PID = %ld, waiting for it to terminate...\n",
-		   (long)getpid(), (long)pid_b);
-	pid_b = wait(&status);
-	explain_wait_status(pid_b, status);
-	printf("A with PID = %ld is ready to terminate...\nA: Exiting...\n", (long)getpid());
-	exit(EXIT_A);
+  printf(
+      "Parent A, PID = %ld: Created child B with PID = %ld, waiting for it to "
+      "terminate...\n",
+      (long)getpid(), (long)pid_b);
+  pid_b = wait(&status);
+  explain_wait_status(pid_b, status);
+  printf("A with PID = %ld is ready to terminate...\nA: Exiting...\n",
+         (long)getpid());
+  exit(EXIT_A);
 }
 
 /*
@@ -119,43 +122,35 @@ void fork_procs(void)
  *      use wait_for_ready_children() to wait until
  *      the first process raises SIGSTOP.
  */
-int main(void)
-{
-	pid_t pid;
-	int status;
+int main(void) {
+  pid_t pid;
+  int status;
 
-	/* Fork root of process tree */
-	pid = fork();
-	if (pid < 0)
-	{
-		perror("main: fork");
-		exit(1);
-	}
-	if (pid == 0)
-	{
-		/* Child */
-		fork_procs();
-		exit(1);
-	}
+  /* Fork root of process tree */
+  pid = fork();
+  if (pid < 0) {
+    perror("main: fork");
+    exit(1);
+  }
+  if (pid == 0) {
+    /* Child */
+    fork_procs();
+    exit(1);
+  }
 
-	/*
-	 * Father
-	 */
-	/* for ask2-signals */
-	/* wait_for_ready_children(1); */
+  /*
+   * Father
+   */
 
-	/* for ask2-{fork, tree} */
-	sleep(SLEEP_TREE_SEC);
+  /* for ask2-{fork, tree} */
+  sleep(SLEEP_TREE_SEC);
 
-	/* Print the process tree root at pid */
-	show_pstree(pid);
+  /* Print the process tree root at pid */
+  show_pstree(pid);
 
-	/* for ask2-signals */
-	/* kill(pid, SIGCONT); */
+  /* Wait for the root of the process tree to terminate */
+  pid = wait(&status);
+  explain_wait_status(pid, status);
 
-	/* Wait for the root of the process tree to terminate */
-	pid = wait(&status);
-	explain_wait_status(pid, status);
-
-	return 0;
+  return 0;
 }
--- Lab2/lab2/forktree/ask2-pipes.c
+++ Lab2/lab2/forktree/ask2-pipes.c
@@ -1,10 +1,10 @@
-#include <unistd.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <signal.h>
-#include <string.h>
+#include <unistd.h>
 
 #include "proc-common.h"
 #include "tree.h"
@@ -12,110 +12,96 @@
 #define SLEEP_PROC_SEC 10
 #define SLEEP_TREE_SEC 3
 
-void fork_procs(struct tree_node *node, int pfd[])
-{
-    pid_t child;
-    int status;
-    int i;
+void fork_procs(struct tree_node *node, int pfd[]) {
+  pid_t child;
+  int status;
+  int i;
 
-    change_pname(node->name);
-    printf("Proccess %s has PID = %ld\n", node->name, (long)getpid());
+  change_pname(node->name);
+  printf("Proccess %s has PID = %ld\n", node->name, (long)getpid());
 
-    if (node->nr_children == 0)
-    {
-        printf("%s: Sleeping...\n", node->name);
-        sleep(SLEEP_PROC_SEC);
-        printf("%s: Done Sleeping...\n", node->name);
+  if (node->nr_children == 0) {
+    printf("%s: Sleeping...\n", node->name);
+    sleep(SLEEP_PROC_SEC);
+    printf("%s: Done Sleeping...\n", node->name);
 
-        if (close(pfd[0]) < 0)
-        {
-            perror("closing pipe");
-            exit(1);
-        }
-
-        int val = atoi(node->name);
-
-        if (write(pfd[1], &val, sizeof(val)) != sizeof(int))
-        {
-            perror("writing in pipe");
-            exit(1);
-        }
+    if (close(pfd[0]) < 0) {
+      perror("closing pipe");
+      exit(1);
     }
-    else
-    {
-        int operators[2] = {0, 0};
-        int newpipe[2];
 
-        if (pipe(newpipe) < 0)
-        {
-            perror("pipe");
-            exit(1);
-        }
+    int val = atoi(node->name);
 
-        for (i = 0; i < 2; ++i)
-        {
-            fprintf(stderr, "Parent %s, PID = %ld: Creating child %s\n", node->name, (long)getpid(), node->children[i].name);
-            child = fork();
-            if (child < 0)
-            {
-                /* fork failed */
-                perror("Error at children");
-                exit(1);
-            }
-
-            if (child == 0)
-            {
-                fork_procs(&node->children[i], newpipe);
-            }
-        }
-
-        if (close(newpipe[1]) < 0)
-        {
-            perror("closing pipe");
-            exit(1);
-        }
-
-        for (i = 0; i < 2; i++)
-        {
-            if (read(newpipe[0], &operators[i], sizeof(operators[i])) != sizeof(operators[i]))
-            {
-                perror("Reading pipe");
-                exit(1);
-            }
-        }
-
-        int res;
-        if (!strcmp(node->name, "+"))
-        {
-            res = operators[0] + operators[1];
-            printf("Current operation is: %d + %d = %d\n", operators[0], operators[1], res);
-        }
-        else
-        {
-            res = operators[0] * operators[1];
-            printf("Current operation is: %d * %d = %d\n", operators[0], operators[1], res);
-        }
-
-        if (close(pfd[0]) < 0)
-        {
-            perror("closing pipe");
-            exit(1);
-        }
-
-        if (write(pfd[1], &res, sizeof(res)) != sizeof(res))
-        {
-            perror("writing in pipe");
-            exit(1);
-        }
-
-        for (i = 0; i < 2; ++i)
-        {
-            child = wait(&status);
-            explain_wait_status(child, status);
-        }
+    if (write(pfd[1], &val, sizeof(val)) != sizeof(int)) {
+      perror("writing in pipe");
+      exit(1);
     }
-    printf("%s with PID = %ld is ready to terminate...\n%s: Exiting...\n", node->name, (long)getpid(), node->name);
-    exit(0);
+  } else {
+    int operators[2] = {0, 0};
+    int newpipe[2];
+
+    if (pipe(newpipe) < 0) {
+      perror("pipe");
+      exit(1);
+    }
+
+    for (i = 0; i < 2; ++i) {
+      fprintf(stderr, "Parent %s, PID = %ld: Creating child %s\n", node->name,
+              (long)getpid(), node->children[i].name);
+      child = fork();
+      if (child < 0) {
+        /* fork failed */
+        perror("Error at children");
+        exit(1);
+      }
+
+      if (child == 0) {
+        fork_procs(&node->children[i], newpipe);
+      }
+    }
+
+    if (close(newpipe[1]) < 0) {
+      perror("closing pipe");
+      exit(1);
+    }
+
+    for (i = 0; i < 2; i++) {
+      if (read(newpipe[0], &operators[i], sizeof(operators[i])) !=
+          sizeof(operators[i])) {
+        perror("Reading pipe");
+        exit(1);
+      }
+    }
+
+    int res;
+    if (!strcmp(node->name, "+")) {
+      res = operators[0] + operators[1];
+      printf("Current operation is: %d + %d = %d\n", operators[0], operators[1],
+             res);
+    } else {
+      res = operators[0] * operators[1];
+      printf("Current operation is: %d * %d = %d\n", operators[0], operators[1],
+             res);
+    }
+
+    if (close(pfd[0]) < 0) {
+      perror("closing pipe");
+      exit(1);
+    }
+
+    if (write(pfd[1], &res, sizeof(res)) != sizeof(res)) {
+      perror("writing in pipe");
+      exit(1);
+    }
+
+    for (i = 0; i < 2; ++i) {
+      child = wait(&status);
+      explain_wait_status(child, status);
+    }
+  }
+  printf("%s with PID = %ld is ready to terminate...\n%s: Exiting...\n",
+         node->name, (long)getpid(), node->name);
+  exit(0);
 }
 
 /* The initial process forks the root of the process tree,
@@ -129,63 +115,57 @@ void fork_procs(struct tree_node *node, int pfd[])
  *      use wait_for_ready_children() to wait until
  *      the first process raises SIGSTOP.
  */
-int main(int argc, char *argv[])
-{
-    pid_t pid;
-    int status;
-    int pfd[2];
-    struct tree_node *root;
+int main(int argc, char *argv[]) {
+  pid_t pid;
+  int status;
+  int pfd[2];
+  struct tree_node *root;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s <input_tree_file>\n\n", argv[0]);
-        exit(1);
-    }
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <input_tree_file>\n\n", argv[0]);
+    exit(1);
+  }
 
-    root = get_tree_from_file(argv[1]);
-    printf("Constructing the following process tree:\n");
-    print_tree(root);
+  root = get_tree_from_file(argv[1]);
+  printf("Constructing the following process tree:\n");
+  print_tree(root);
 
-    printf("Parent: Creating pipe...\n");
-    if (pipe(pfd) < 0)
-    {
-        perror("pipe");
-        exit(1);
-    }
+  printf("Parent: Creating pipe...\n");
+  if (pipe(pfd) < 0) {
+    perror("pipe");
+    exit(1);
+  }
 
-    printf("Parent: Creating child...\n");
-    pid = fork();
-    if (pid < 0)
-    {
-        /* fork failed */
-        perror("main: fork");
-        exit(1);
-    }
-    if (pid == 0)
-    {
-        fork_procs(root, pfd);
-        exit(1);
-    }
+  printf("Parent: Creating child...\n");
+  pid = fork();
+  if (pid < 0) {
+    /* fork failed */
+    perror("main: fork");
+    exit(1);
+  }
+  if (pid == 0) {
+    fork_procs(root, pfd);
+    exit(1);
+  }
 
-    /*
-	 * In parent process.
-	 */
-    sleep(SLEEP_TREE_SEC);
-    show_pstree(pid);
+  /*
+   * In parent process.
+   */
+  sleep(SLEEP_TREE_SEC);
+  show_pstree(pid);
 
-    int res;
-    if (read(pfd[0], &res, sizeof(res)) != sizeof(res))
-    {
-        perror("read from pipe");
-        exit(1);
-    }
+  int res;
+  if (read(pfd[0], &res, sizeof(res)) != sizeof(res)) {
+    perror("read from pipe");
+    exit(1);
+  }
 
-    /* Print the process tree root at pid */
+  /* Print the process tree root at pid */
 
-    pid = wait(&status);
-    explain_wait_status(pid, status);
+  pid = wait(&status);
+  explain_wait_status(pid, status);
 
-    printf("The final result is %d\n", res);
+  printf("The final result is %d\n", res);
 
-    return 0;
+  return 0;
 }
